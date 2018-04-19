310fd891a51ce3c8d7242749fc8709f7cbe80fba
mzz <m@mzz.pub>
2018-04-19 13:44:52 +0800

    搞了一下钢筋混泥土

--- src/main.js
+++ src/main.js
@@ -3,13 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+import FastClick from 'fastclick'
+
+Vue.use(Element)
 
 Vue.config.productionTip = false
 
+FastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
 })
