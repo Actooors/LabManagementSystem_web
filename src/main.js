5be13394043548148f8d8f0afad522bee0378272
mzz2017 <m@mzz.pub>
2018-05-03 12:49:02 +0800

    解决冲突

--- src/main.js
+++ src/main.js
@@ -3,23 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-<<<<<<< HEAD
-// 引入element-ui
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-=======
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import FastClick from 'fastclick'
 import 'normalize.css'
 import VueLazyload from 'vue-lazyload'
->>>>>>> d3b93b6aa3f8ff927e280e11acae8978bc037d92
+
 
 Vue.use(VueLazyload)
 
 Vue.use(Element)
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+
 
 FastClick.attach(document.body)
 /* eslint-disable no-new */
